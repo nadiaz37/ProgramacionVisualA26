769409deb7cf0caa8a73a08ce09f8ef1e65daffc
wgaibor <wgaibor@teclemas.edu.ec>
2026-05-22 19:33:35 -0500

    Finaliza app de hola mundo

--- holamundo/src/main/java/com/holamundo/SaludoController.java
+++ holamundo/src/main/java/com/holamundo/SaludoController.java
@@ -2,6 +2,7 @@ package com.holamundo;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
 
 public class SaludoController {
@@ -11,7 +12,16 @@ public class SaludoController {
 
     @FXML
     private void mostrarMensaje(ActionEvent event) {
-
+        // Crear una alerta para mostrar el mensaje
+        String mensaje = txtMensaje.getText();
+        if (mensaje.isEmpty()) {    
+            mensaje = "¡Hola, Mundo!";
+        }
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Mensaje");
+        alert.setHeaderText(null);
+        alert.setContentText(mensaje);
+        alert.showAndWait();
     }
 
     @FXML
